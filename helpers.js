fbc2ba69926374749d7ce44043c013f78884df32
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2016-11-17 20:15:40 +0530

    Parse cron file + Append url

---
+++ helpers.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const {red, green} = require('colors');
+const md5 = require('blueimp-md5');
+
+const baseUrl = 'https://cronduty.com';
+
+/* Check if cron file exists */
+let fileExists = (path) => {
+    try {
+        let stat = fs.statSync(path);
+    } catch (e) {
+        error(`${path} does not exist`);
+    }
+};
+
+/* Pretty error message + exit */
+let error = (message) => {
+    console.log(`\n${red(message)}\n`);
+    process.exit(1);
+};
+
+/* Pretty success message */
+let success = (message) => {
+    console.log(`\n${green(message)}\n`);
+};
+
+/* Generate an unique id for each job
+ *
+ * Based on userId + cron string
+ */
+let getUniqueId = (userId, jobString) => {
+    let randomDigits = Math.random().toString(36).slice(2, 6);
+    let combination = userId + jobString + randomDigits;
+    let uniqueId = md5(combination).slice(0, 4);
+    return uniqueId;
+};
+
+/* Attach curl url to command */
+let attachMonitoring = (userId, job) => {
+    let uniqueId = getUniqueId(userId, job.toString());
+
+    let command = `curl ${baseUrl}/start/${uniqueId}`;
+    command += ` || `;
+    command += job.command();
+    command += ` && `;
+    command += `curl ${baseUrl}/done/${uniqueId}`;
+
+    return job.command(command);
+};
+
+module.exports = {
+    fileExists,
+    error,
+    success,
+    attachMonitoring
+};
+
---
+++ index.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+const crontab = require('crontab');
+const argv = require('yargs-parser')(process.argv.slice(2));
+const {fileExists, error, success, attachMonitoring} = require('./helpers');
+
+/* Is file passed as argument? */
+let filePath = argv._[0] || '';
+
+/* If yes, make sure the file exists */
+if (filePath) fileExists(filePath);
+
+/* Register user - get back id */
+let userId = 1;
+
+/* Setup jobs
+ *
+ * Load cron file
+ * Append urls
+ * Push all jobs to api
+ * Save to cron file
+ *
+ */
+
+crontab.load('', filePath, (err, crons) => {
+    if (err) error('Could not parse cron file');
+
+    let jobs = crons.jobs();
+
+    if (!jobs.length) error('No jobs found');
+    else success(`Found ${jobs.length} jobs`);
+
+    for (let job of jobs) attachMonitoring(userId, job);
+    crons.save();
+});
+
+
